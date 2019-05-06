5dea08ab8ce8ecde5b500dd5e8e8966c8720e191
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2019-05-06 20:56:40 +1000

    Check for pre-existing preinstall hook

--- install.js
+++ install.js
@@ -11,19 +11,19 @@ console.log(nodeModules);
 if (!nodeModules) return; // No parent node_modules
 
 const hooksDir = path.resolve(nodeModules, '.hooks');
-console.log(hooksDir);
 
 if (!fs.existsSync(hooksDir)) {
-    console.log("Making hooks dir");
     fs.mkdirSync(hooksDir);
 }
 
 const symlinkPath = path.resolve(hooksDir, "preinstall");
 const symlinkTarget = path.relative(hooksDir, './hooks/preinstall');
 
-console.log(symlinkPath, "-->", symlinkTarget);
-
-fs.symlinkSync(symlinkTarget, symlinkPath);
+if (fs.existsSync(hooksDir)) {
+    console.warn("npm preinstall hook already exists, not overwriting.");
+} else {
+    fs.symlinkSync(symlinkTarget, symlinkPath);
+}
 
 // Run initial version check
 require('./');
