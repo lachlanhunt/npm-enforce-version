8b1d849a9653a4f74b202964c17aa965afd6d960
Lachlan Hunt <lachlan.hunt@lachy.id.au>
2019-05-06 20:48:26 +1000

    Create install hook to setup preinstall hook

--- install.js
+++ install.js
@@ -1,9 +1,29 @@
-const check = require('./check');
+// Setup preinstall hook
 
-try {
-    const version = process.argv.slice(2)[0];
-    return check(version);
-} catch(err) {
-    console.error(err);
-    process.exit(1);
+const fs = require('fs');
+const path = require('path');
+const findNodeModules = require('find-node-modules');
+
+const [, nodeModules] = findNodeModules();
+
+console.log(nodeModules);
+
+if (!nodeModules) return; // No parent node_modules
+
+const hooksDir = path.resolve(nodeModules, '.hooks');
+console.log(hooksDir);
+
+if (!fs.existsSync(hooksDir)) {
+    console.log("Making hooks dir");
+    fs.mkdirSync(hooksDir);
 }
+
+const symlinkPath = path.resolve(hooksDir, "preinstall");
+const symlinkTarget = path.relative(hooksDir, './hooks/preinstall');
+
+console.log(symlinkPath, "-->", symlinkTarget);
+
+fs.symlinkSync(symlinkTarget, symlinkPath);
+
+// Run initial version check
+require('./');
